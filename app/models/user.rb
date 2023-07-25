ee5a7f30f3f330bb9bb35e0cd814a94253bbb2d3
George Francis <georgeemadfrancis@gmail.com>
2023-07-25 15:56:26 +0300

    added admin and users pages to the admin panel

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :docs
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["email", "encrypted_password", "reset_password_token", "reset_password_sent_at", "remember_created_at"]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    ["docs"]
+  end
 end
