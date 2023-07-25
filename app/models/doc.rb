2495089423ca005e43aad022414b76ee4db3edc7
George Francis <georgeemadfrancis@gmail.com>
2023-07-25 15:54:11 +0300

    added the admin panel and added the docs model to it

--- app/models/doc.rb
+++ app/models/doc.rb
@@ -1,3 +1,11 @@
 class Doc < ApplicationRecord
     belongs_to :user
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["content", "created_at", "id", "title", "updated_at", "user_id"]
+      end
+
+    def self.Ransackable_associations(auth_object = nil)
+        ["user_id", "user"]
+    end
 end
